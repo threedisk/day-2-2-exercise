06455c1883d0ba95c8e9a8333862ecee40a09f4b
threedisk <threedisk@gmail.com>
2020-12-29 10:56:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#Rumus = massa / tinggi**2
+new_height = float(height)
+new_weight = float(weight)
+bmi = new_weight / new_height**2
+#bmi_as_int = int(bmi)
+#print(round(bmi_as_int,2))
+print(round(bmi,2))
+
+
+
+
+
+
+
+
+
+
